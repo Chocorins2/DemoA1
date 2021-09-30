6fa3e7aede7b7c7aa1f8623c60ba4778914773be
Rosa Hongoc <chocorins2@gmail.com>
2021-09-30 16:06:20 -0400

    done navbar

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,19 @@ exports.default = router;
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
+router.get('/home', function (req, res, next) {
+    res.render('index', { title: 'Home' });
+});
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'About' });
+});
+router.get('/project', function (req, res, next) {
+    res.render('index', { title: 'Project' });
+});
+router.get('/services', function (req, res, next) {
+    res.render('index', { title: 'Services' });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact' });
+});
 //# sourceMappingURL=index.js.map
