6b89b5b3abd2e19cb195d63f1d9a4ae242b08f8b
Rosa Hongoc <chocorins2@gmail.com>
2021-09-30 15:20:13 -0400

    added bootstrap and fontawesome

---
+++ Client/Scripts/app.ts
@@ -0,0 +1,10 @@
+// IIFE -- Immediately Invoked Function Express
+"use strict";
+(function()
+{
+    function Start(): void
+    {
+        console.log("App Started");
+    }
+    window.addEventListener("load", Start);
+})
